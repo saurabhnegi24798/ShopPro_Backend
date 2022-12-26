da70f90d0865adc2edcdbf237d4adbe685c72948
saurabhnegi24798 <saurabhnegi24798@gmail.com>
2022-12-26 23:28:09 +0530

    commit

---
+++ build/precache-manifest.8cdfb314c7f47929383777132ffbd945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2323fc574156c63244ce8d541b5a5c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23354650b916fefdf192",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "0fd34b4eceea50136d8b",
+    "url": "/static/js/2.4f0df190.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4f0df190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23354650b916fefdf192",
+    "url": "/static/js/main.35ddca7d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import colors from 'colors'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import cors from 'cors'
+// import cors from 'cors'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
@@ -17,10 +17,10 @@ connectDB()
 
 const app = express()
 
-app.use(cors({
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}))
+// app.use(cors({
+//   origin: '*',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }))
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -40,19 +40,17 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   )
-// } else {
-
-// }
-
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
 
 app.use(notFound)
 app.use(errorHandler)
